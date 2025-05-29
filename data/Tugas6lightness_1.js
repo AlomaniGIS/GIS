e84f2c618e5935d87d027dff68dd183734fb65b2
EndTheTask <ilhammhd551@gmail.com>
2025-05-29 13:56:39 +0700

    LFS

---
+++ data/Tugas6lightness_1.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4fc4f7d450f377f0fc9091e39a4dfa7d1585890fa84943e631dc1c278d7d48f7
+size 442032364
---
+++ data/Tugas8Koordinatdan9Foto_2.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:29fa326c405c8370d21ece402101f9d4965079fad904bd6ed99f7923fbcc6b28
+size 149575
